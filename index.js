dd40a25597c40161568f745caf3c4b5b2cc59f8d
notcoe <siriphon_not@hotmail.com>
2014-11-05 03:27:27 +0700

    Add book info

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/api/book',function(reg,res){
            {title: 'Angular' ,price: 800},
            {title: 'Node.jd' ,price: 600},
            {title: 'CSS' ,price: 400}
+           {title: 'JavaScript' ,price: 900}
+           {title: 'MongoDB' ,price: 700}
+           {title: 'HTML5' ,price: 1200}
       ];
       res.send(books);
 })
