6d4ff02ec25b98ba625622903e38a330ead73236
notcoe <siriphon_not@hotmail.com>
2014-11-05 03:39:01 +0700

    Edit book info

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.get('/api/book',function(reg,res){
       var books = [
            {title: 'Angular' ,price: 800},
            {title: 'Node.jd' ,price: 600},
-           {title: 'CSS' ,price: 400}
-           {title: 'JavaScript' ,price: 900}
-           {title: 'MongoDB' ,price: 700}
+           {title: 'CSS' ,price: 400},
+           {title: 'JavaScript' ,price: 900},
+           {title: 'MongoDB' ,price: 700},
            {title: 'HTML5' ,price: 1200}
       ];
       res.send(books);
