7fc483753a03e1f084c1c23775350bdcce7b9e3c
notcoe <siriphon_not@hotmail.com>
2014-11-13 02:59:22 +0700

    create insert data form

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//server
+
 var express = require('express')
-var mongojs = require('mongojs')     //
+var mongojs = require('mongojs')     //incliude mongojs
 var bodyParser = require('body-parser')
 var app = express()
+
 var db = mongojs('my_server',['book']);   //connect database
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/api/book',function(reg,res){
+app.get('/api/book',function(reg,res){  //sent data from server to app.js (pass docs) 
 
       db.book.find({},function(err,docs){   //query database
            res.send(docs);
@@ -16,7 +19,7 @@ app.get('/api/book',function(reg,res){
 })
 
 app.post('/api/book',function(req,res){
-    db.book.insert(req.body,function(err,docs){   //query database
+    db.book.insert(req.body,function(err,docs){   //query database keep data in database
            res.send(docs);
       });
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,27 @@
+//javascript connect with server (index.js)
+
 angular.module("myApp",[])
-.controller('mainCtrl', function($scope,$http){
+
+.controller('mainCtrl', function($scope,$http,$window){
 	$scope.books  = [];
-	$http.get('api/book').success(function(data){
+    
+
+	$http.get('api/book').success(function(data){  //call data from database
          $scope.books = data;
 	})
+    
+    $scope.submit = function(){
+        $http.post('api/book',{     //keep data in database
+          id : $scope.id,
+          title : $scope.title,
+          price : $scope.price
+            
+	    })
+         .success(function(data){  
+             console.log("seccess");
+             $window.location.reload();
+         })
+
+    };
+	
 })
