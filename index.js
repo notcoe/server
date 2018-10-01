d9a351263b309acea27fe809c624bfa6c50110c9
Thanwa JDR <thanwa.jdr@gmail.com>
2018-10-01 15:55:10 +0700

    use ES6 style

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 //server
 
-var express = require('express')
-var mongojs = require('mongojs')     //incliude mongojs
-var bodyParser = require('body-parser')
-var app = express()
+const express = require('express')
+const mongojs = require('mongojs') //incliude mongojs
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = process.env.PORT || '3000';
 
-var db = mongojs('my_server',['book']);   //connect database
+const db = mongojs('my_server', ['book']); //connect database
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/api/book',function(reg,res){  //sent data from server to app.js (pass docs) 
+app.get('/api/book', function (reg, res) { //sent data from server to app.js (pass docs) 
 
-      db.book.find({},function(err,docs){   //query database
-           res.send(docs);
-      });
-      
+    db.book.find({}, function (err, docs) { //query database
+        res.send(docs);
+    });
+
 })
 
-app.post('/api/book',function(req,res){
-    db.book.insert(req.body,function(err,docs){   //query database keep data in database
-           res.send(docs);
-      });
+app.post('/api/book', function (req, res) {
+    db.book.insert(req.body, function (err, docs) { //query database keep data in database
+        res.send(docs);
+    });
 
 });
 
-var server = app.listen(3000, function () {
-  console.log("server is running")
-
+const server = app.listen(PORT, function () {
+    console.log(`server is running at http://localhost:${PORT}`);
 })
-
